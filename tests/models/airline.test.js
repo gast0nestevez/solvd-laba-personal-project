8e60d81a9f7d9ffadc7959b4c9153cce947f4391
gast0nestevez <gastonestevez08@gmail.com>
2025-09-27 01:44:45 -0300

    fix

--- tests/models/airline.test.js
+++ tests/models/airline.test.js
@@ -1,17 +1,8 @@
 import { jest } from '@jest/globals'
-
-jest.mock('../../src/utils/db.js', () => {
-  return {
-    __esModule: true,
-    default: {
-      query: jest.fn(),
-      end: jest.fn()
-    }
-  }
-})
-
 import pool from '../../src/utils/db.js'
 import { Airline } from '../../src/models/airline.model.js'
+
+jest.mock('../../src/utils/db.js')
 
 describe('Airline model', () => {
   beforeEach(() => {
